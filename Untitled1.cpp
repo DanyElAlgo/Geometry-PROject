79f378ea38c3beed044a136f1b392439497cc3be
DanyElAlgo <eldanielitu@gmail.com>
2023-03-31 13:06:24 -0400

    aaaaaa

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -3,16 +3,20 @@ using namespace std;
 int main () {
 	
 	const double PI = 3.1416;
-	double radio, longitud;
+	double radio, longitud, area, volumen;
 	
-	cout << "ingrese el valor del radio: ";
+	cout << "Ingrese el valor del radio: ";
 	cin >> radio;
 	
 	if (radio >= 0) {
 		
 		longitud = 2 * PI * radio;
+		area = PI * radio * radio;
+		volumen = (4/3) * PI * radio * radio * radio;
 		
-		cout << "la longitud de la circunferencia es: " << longitud << endl;
+		cout << "La longitud de la circunferencia es: " << longitud << endl;
+		cout << "El area del círculo es: " << area << endl;
+		cout << "El volumen de la esfera es: " << volumen << endl;
 	}
 	
 }
